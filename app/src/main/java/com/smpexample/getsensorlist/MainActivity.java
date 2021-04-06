21330ec7899730f9eb6883684c6c7c7ed837eb0d
CHOI MIN JIN <chlalswls2409@naver.com>
2021-04-06 10:20:11 +0900

    get sensor list

--- app/src/main/java/com/smpexample/getsensorlist/MainActivity.java
+++ app/src/main/java/com/smpexample/getsensorlist/MainActivity.java
@@ -2,13 +2,31 @@ package com.smpexample.getsensorlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mTxtSensors;
+    SensorManager sensorMgr;
+    List<Sensor> sensorList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTxtSensors = findViewById(R.id.txtSensors);
+        sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        sensorList = sensorMgr.getSensorList(Sensor.TYPE_ALL);
+        mTxtSensors.append("(# Sensors : " + sensorList.size() + ")\n\n");
+        for(Sensor sensor : sensorList) {
+            mTxtSensors.append("(# Sensors name : " + sensor.getName() + "\n");
+            mTxtSensors.append("(# Sensors type : " + sensor.getType() + "\n\n");
+        }
     }
 }
